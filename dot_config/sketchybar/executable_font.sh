442087452a4a22e556311dcf092beb7efeb9055c
Marc Ignacio <padi@users.noreply.github.com>
2026-03-01 21:26:09 +0800

    sketchybar: move color and font config to separate file for later themes

---
+++ dot_config/sketchybar/executable_font.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# -- Default
+# export ICON_FONT="Hack Nerd Font:Bold:17.0"
+# export LABEL_FONT="Hack Nerd Font:Bold:14.0"
+
+# -- Custom
+export ICON_FONT="Hack Nerd Font:Bold:17.0"
+export LABEL_FONT="SF Pro:Semibold:14.0"
